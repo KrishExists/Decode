31a1463dd152f6e33d904922d4e180e6b36f66c9
ftcbeta <ftcbeta@gmail.com>
2025-09-22 17:44:30 -0400

    Yes, everything works! MecanumTeleop is now integrated with April Tag. Yet to be tested. April tag detection is sucessful.Done. Done 9/13. Doneagain. Honestly dont know what happened. Fixed the. Hopefully fixed. Added new code proto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterProto.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class BallShooterProto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor shooter1 = hardwareMap.dcMotor.get("armMotorLeft");
+        DcMotor shooter2 = hardwareMap.dcMotor.get("armMotorRight");
+       // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+      frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter2.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter1.setDirection(DcMotorSimple.Direction.REVERSE);
+        double speed = 0.0;
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+            if(gamepad1.dpad_down){
+                if(speed<0.05){
+                    continue;
+                }else{
+                    speed-=0.05;
+                }
+            }
+            if(gamepad1.dpad_up){
+                if(speed>0.096){
+                    continue;
+                }else{
+                    speed+=0.05;
+                }
+            }
+            if(gamepad1.dpad_left){
+                if(speed>0.91){
+                    continue;
+                }else{
+                    speed +=0.1;
+                }
+            }
+            if(gamepad1.dpad_right){
+                if(speed<0.09){
+                    continue;
+                }else{
+                    speed -=0.1;
+                }
+            }
+            if(gamepad1.a){
+                speed = 0.8;
+            }
+            if(gamepad1.b){
+                speed = 0;
+            }
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+            shooter2.setPower(speed);
+            shooter1.setPower(speed);
+        }
+    }
+}
