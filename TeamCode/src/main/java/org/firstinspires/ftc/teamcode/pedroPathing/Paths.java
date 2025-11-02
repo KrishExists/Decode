1bb541c4cdc0969af41effe8bf4f107d2982f83e
ftcbeta <ftcbeta@gmail.com>
2025-11-02 15:47:28 -0500

    basic drivetrain is done. AutoAlign and Switch is in this code. 11/1. 11/2.some pedro is done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Paths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Paths.java
@@ -11,9 +11,15 @@ public class Paths {
     public PathChain Path1;
     public PathChain Path2;
     public PathChain Path3;
+    public PathChain Path4;
 
     public Paths(Follower follower) {
         Path1 = follower
+                .pathBuilder()
+                .addPath(
+                .new BezierLine(follower)
+                )
+        Path2 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(70.000, 80.000), new Pose(34.120, 83.277))
@@ -21,7 +27,7 @@ public class Paths {
                 .setLinearHeadingInterpolation(Math.toRadians(67), Math.toRadians(120))
                 .build();
 
-        Path2 = follower
+        Path3 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(34.120, 83.277), new Pose(16.578, 84.434))
@@ -29,7 +35,7 @@ public class Paths {
                 .setLinearHeadingInterpolation(Math.toRadians(120), Math.toRadians(170))
                 .build();
 
-        Path3 = follower
+        Path4 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(16.578, 84.434), new Pose(70.000, 80.000))
