5921152225bc44b1a2066b9f746b8cdc03a4a483
Krish Sinha <krishworld432@gmail.com>
2026-01-07 12:21:31 -0500

    Created the Simply World Record Paths. Probably not final due to the scuffed path. But yeah i think it might work.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auto/RedSimplySynchronization.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opMode.auto;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.TelemetryManager;
+import com.bylazar.telemetry.PanelsTelemetry;
+
+import org.firstinspires.ftc.teamcode.opMode.auto.pathsAuto.SimplySyncPaths;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.geometry.Pose;
+
+
+@Autonomous(name = "Simply Sync (Red)", group = "Autonomous")
+@Configurable // Panels
+public class RedSimplySynchronization extends OpMode {
+    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+    public Follower follower; // Pedro Pathing follower instance
+    private int pathState; // Current autonomous path state (state machine)
+    private SimplySyncPaths paths; // Paths defined in the Paths class
+
+    @Override
+    public void init() {
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
+
+        paths = new SimplySyncPaths(follower); // Build paths
+
+        panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
+    }
+
+    @Override
+    public void loop() {
+        follower.update(); // Update Pedro Pathing
+        pathState = autonomousPathUpdate(); // Update autonomous state machine
+
+        // Log values to Panels and Driver Station
+        panelsTelemetry.debug("Path State", pathState);
+        panelsTelemetry.debug("X", follower.getPose().getX());
+        panelsTelemetry.debug("Y", follower.getPose().getY());
+        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
+        panelsTelemetry.update(telemetry);
+    }
+
+
+
+
+
+    public int autonomousPathUpdate() {
+        // Event markers will automatically trigger at their positions
+        // Make sure to register NamedCommands in your RobotContainer
+        return pathState;
+    }
+
+
+}
