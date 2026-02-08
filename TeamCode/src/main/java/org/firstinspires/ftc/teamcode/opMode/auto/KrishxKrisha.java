6f9ae13d95d3a7993fdec21456005855046aade2
Aditya Navale <adityaanavale@gmail.com>
2026-02-07 23:11:33 -0500

    jk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auto/KrishxKrisha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auto/KrishxKrisha.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.opMode.auto;
 
+import androidx.annotation.NonNull;
+
 public class KrishxKrisha {
+    public static void main(String[] args){
+        System.out.println("Krish likes Krisha");
+    }
 }
